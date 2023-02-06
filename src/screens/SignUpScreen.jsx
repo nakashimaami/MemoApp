c4d86eafafc49144fcbc46648ff3b4ce98b0cd80
nakashima <nakashima.timecapsule@gmail.com>
2023-02-06 17:20:59 +0900

    Implement signup and login

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import {
-  View, Text, TextInput, StyleSheet, TouchableOpacity,
+  View, Text, TextInput, StyleSheet, TouchableOpacity,Alert,
 } from 'react-native';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 import Button from '../components/Button';
 
@@ -9,6 +11,22 @@ export default function SignUpScreen(props) {
   const { navigation } = props; 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function handlePress() {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+       .then((userCredential) => {
+         const { user } = userCredential;
+         console.log(user.uid);
+         navigation.reset({
+            index: 0,
+            routes: [{ name: 'MemoList' }],
+          }); 
+       })
+       .catch((error) => {
+        console.log(error.code, error.message);
+        Alert.alert(error.code);
+       });
+  }
 
   return (
     <View style={styles.container}>
@@ -34,12 +52,7 @@ export default function SignUpScreen(props) {
         />
         <Button 
         label="Submit"
-        onPress={ () => { 
-            navigation.reset({
-            index: 0,
-            routes: [{ name: 'MemoList' }],
-            }); 
-        }}
+        onPress={handlePress}
         />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
