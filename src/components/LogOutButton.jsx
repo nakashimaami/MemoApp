811da2fdaccc87ee04d44678d382cb91dc71271f
nakashima <nakashima.timecapsule@gmail.com>
2023-02-07 10:24:54 +0900

    Auto login and logout button

---
+++ src/components/LogOutButton.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { useNavigation } from '@react-navigation/native';
+
+export default function LogOutButton() {
+    const navigation = useNavigation();
+    
+    function handlePress() {
+        firebase.auth().signOut()
+        .then(() => {
+            navigation.reset({
+                index: 0,
+                routes: [{ name: 'LogIn' }],
+            });
+        })
+        .catch(() => {
+            Alert.alert('ログアウトに失敗しました');
+        });
+    }
+
+
+    return (
+      <TouchableOpacity onPress={handlePress} style={styles.container}>
+        <Text style={styles.label}>
+            ログアウト
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+  
+
+  const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 12,
+        paddingVertical: 4,
+    },
+    label: {
+        fontSize: 14,
+        color: 'rgba(134,33,33,0.7)',
+    },
+  });
+  
