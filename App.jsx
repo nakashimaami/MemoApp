a28c290bb427628b6f73578ab4066fdef4803458
nakashima <nakashima.timecapsule@gmail.com>
2023-02-10 13:49:45 +0900

    Fix3

--- App.jsx
+++ App.jsx
@@ -12,9 +12,16 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LogInScreen from './src/screens/LogInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 
-import { firebaseConfig } from './.env';
-
 require('firebase/firestore');
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDF2yLa5RbKycltFDIR8O0nl8zHHzyxMhc',
+  authDomain: 'memoapp2-6e3d9.firebaseapp.com',
+  projectId: 'memoapp2-6e3d9',
+  storageBucket: 'memoapp2-6e3d9.appspot.com',
+  messagingSenderId: '1068117841065',
+  appId: '1:1068117841065:web:ce7428bad41581b6c805b8',
+};
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
