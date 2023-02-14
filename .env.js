b8b599105600929468d887fbae650c2739e195d9
nakashima <nakashima.timecapsule@gmail.com>
2023-02-14 09:43:57 +0900

    Fix5

---
+++ .env.js
@@ -0,0 +1,9 @@
+/* eslint-disable-next-line */
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDF2yLa5RbKycltFDIR8O0nl8zHHzyxMhc',
+  authDomain: 'memoapp2-6e3d9.firebaseapp.com',
+  projectId: 'memoapp2-6e3d9',
+  storageBucket: 'memoapp2-6e3d9.appspot.com',
+  messagingSenderId: '1068117841065',
+  appId: '1:1068117841065:web:ce7428bad41581b6c805b8',
+};
